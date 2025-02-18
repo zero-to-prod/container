992821a06a92efa3349dfb494fa76fa9b5c52733
David Smith <dasmith@zero-to-prod.com>
2025-02-18 06:50:42 -0500

    doc/gen Updated `src/EntryNotFoundException.php`.

--- src/EntryNotFoundException.php
+++ src/EntryNotFoundException.php
@@ -5,6 +5,9 @@ namespace Zerotoprod\Container;
 use Exception;
 use Psr\Container\NotFoundExceptionInterface;
 
+/**
+ * @link https://github.com/zero-to-prod/container
+ */
 class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
 {
     //
