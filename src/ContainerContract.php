3dde31e80953c9b572ec1f1ef0ae4c2a9a65bde2
david <dasmith@zero-to-prod.com>
2025-02-18 06:58:41 -0500

    doc/gen (#3)
    * doc/gen Updated `src/Container.php`.
    
    * doc/gen Updated `src/ContainerContract.php`.
    
    * doc/gen Updated `src/EntryNotFoundException.php`.
    
    * doc/gen Updated `src/Container.php`.

--- src/ContainerContract.php
+++ src/ContainerContract.php
@@ -4,6 +4,9 @@ namespace Zerotoprod\Container;
 
 use Psr\Container\ContainerInterface;
 
+/**
+ * @link https://github.com/zero-to-prod/container
+ */
 interface ContainerContract extends ContainerInterface
 {
 
